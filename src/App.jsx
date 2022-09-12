7a283aaaef83b582dd20978de02d807c93afa130
Camille <83260908+Camille846@users.noreply.github.com>
2022-09-12 15:07:46 -0300

    Feat: React Chat Engine dependencies and import

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
+import { ChatEngine } from 'react-chat-engine';
 
 function App() {
   return (
     <div className="App">
-     
+     <ChatEngine 
+        height="100vh"
+        projectID="977b5dd6-2715-4770-aa71-b50d5349e60a"
+        userName="Maria"
+        userSecret="Maria123"
+     />
     </div>
   )
 }
