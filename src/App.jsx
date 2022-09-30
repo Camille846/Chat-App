965dea6578acb1992e204c71d98421bcae5a8fed
Camille <83260908+Camille846@users.noreply.github.com>
2022-09-30 09:09:33 -0300

    Feat: Login Form Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import './App.css'
 import { ChatEngine } from 'react-chat-engine';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
 function App() {
+  if (!localStorage.getItem('username')) return <LoginForm />
+
   return (
     <div className="App">
      <ChatEngine 
         height="100vh"
         projectID="3cc2f581-7459-4349-9179-70189b0f381a"
-        userName="Papaya"
-        userSecret="Papaya123"
+        userName={localStorage.getItem('username')}
+        userSecret={localStorage.getItem('password')}
         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
      />
     </div>
